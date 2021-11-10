d81cfb810754b314f6b4c4a397e4a0403dfa2984
Valiantsin_Chesnakou <Valiantsin.Chesnakou@gmail.com>
2021-11-10 09:51:45 +0300

    Lesson 12

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -3,6 +3,7 @@ package tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
@@ -21,8 +22,13 @@ public class BaseTest {
 
     @BeforeMethod
     public void initTest() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        if(System.getProperty("browser").equals("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (System.getProperty("browser").equals("edge")) {
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        }
         driver.manage().window().maximize();
         loginPage = new LoginPage(driver);
         productsPage = new ProductsPage(driver);
--- src/test/java/tests/CartTests.java
+++ src/test/java/tests/CartTests.java
@@ -19,7 +19,7 @@ public class CartTests extends BaseTest{
     @Test
     public void addProductToCartWithPageFactoryTest() {
         loginPageFactory.openPage("https://www.saucedemo.com/");
-        loginPageFactory.login("standard_user", "secret_sauce");
+        loginPageFactory.login(System.getProperty("username"), System.getProperty("password"));
         productsPage.addProductToCart("Sauce Labs Bolt T-Shirt");
         productsPage.openPage("https://www.saucedemo.com/cart.html");
         Assert.assertEquals(cartPage.getProductPrice("Sauce Labs Bolt T-Shirt"), "$15.99");
